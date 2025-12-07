6a64cb802c36128065235f1f808c978292f1b062
Matheus Colombo <111023158+mathcolombo@users.noreply.github.com>
2025-12-07 12:24:30 -0300

    GRM-1 - Atualiza classe base Entity para trabalhar com soft delete

--- src/Grimoire.Domain/Shared/Abstractions/Entity.cs
+++ src/Grimoire.Domain/Shared/Abstractions/Entity.cs
@@ -5,8 +5,22 @@ public abstract class Entity
     public int Id { get; protected set; }
     public DateTime CreatedAt { get; protected set; }
     public DateTime UpdatedAt { get; protected set; }
+    public DateTime? DeletedAt { get; protected set; }
+    public bool IsDeleted => DeletedAt != null;
 
     protected Entity() => CreatedAt = DateTime.Now;
 
     public void UpdateTimestamp() => UpdatedAt = DateTime.Now;
+    
+    public void SoftDelete()
+    {
+        DeletedAt = DateTime.Now;
+        UpdatedAt = DateTime.Now;
+    }
+
+    public void UndoDelete()
+    {
+        DeletedAt = null;
+        UpdatedAt = DateTime.Now;
+    }
 }
